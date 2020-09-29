322209c93785b2bf020f533cf6753ec1ae8bc4ec
João Carlos <joaocarloslima@me.com>
2020-09-29 09:54:23 -0300

    :new: CRUD tasks

--- target/classes/data.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO users (name, email, pass) VALUES
-('Joao Carlos', 'joao@gmail.com', '$2a$10$CqzuQA66hFso6.CQt28UR.41QPqq4rNJN30ttsC771gDjYDbn/I5a');
-
-INSERT INTO tasks (title, description, point, status) VALUES
-('Analise', 'analise completa do sistemas', 100, 90),
-('Protype', 'prototipacao das telas', 100, 70),
-('Data base', 'criacao do banco de dados', 50, 10),
-('Teste', 'teste de integração', 80, 20);
